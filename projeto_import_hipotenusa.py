0e520ffb9c02e07dc6f9a613fcb0f6a491ec753d
psilva0 <pedrofsilva1806@gmail.com>
2025-08-31 12:07:14 -0300

    Delete projeto_import_hipotenusa.py

--- projeto_import_hipotenusa.py
+++
@@ -1,7 +0,0 @@
-
-
-import math
-cat1 = float(input('Digite a metragem do cateto adjacente: '))
-cat2 = float(input('Digite a metragem do cateto oposto: '))
-hip = math.hypot(cat1, cat2)
-print('Dado os catetos {} e {} a sua hipotenusa é {}'.format(cat1, cat2, hip))
